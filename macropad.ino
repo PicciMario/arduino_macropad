1c4adf644c63b2488e8d029576434dd58a28f508
mario piccinelli <mario.piccinelli@gmail.com>
2022-09-15 21:22:45 +0200

    codice tastiera, riordino

--- macropad.ino
+++ macropad.ino
@@ -2,51 +2,83 @@
 // baldengineer.com
 // mariopiccinelli.it
 // CC BY-SA 4.0
- 
-// JP1 is an input
-byte rows[] = {5, 6, 7};
+
+#include <Keyboard.h>
+
+// ROWS (inputs)
+byte rows[] = {7, 6, 5};
 const int rowCount = sizeof(rows)/sizeof(rows[0]);
 
-// JP2 and JP3 are outputs
-byte cols[] = {2, 3, 4};
+// COLUMNS (outputs)
+byte cols[] = {2, 4, 3};
 const int colCount = sizeof(cols)/sizeof(cols[0]);
 
+// Current status of switches
 byte keys[colCount][rowCount];
 
-void setup() {
-  Serial.begin(115200);
+char keyValues[3][3] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'}
+};
 
+void setup() {
+  
+  Serial.begin(115200);
+  Keyboard.begin();
+
+  // Set cols (outputs) as inputs
   for(int x=0; x<rowCount; x++) {
-    Serial.print(rows[x]); Serial.println(" as input");
     pinMode(rows[x], INPUT);
   }
 
+  // Set rows as inputs with pullup
   for (int x=0; x<colCount; x++) {
-    Serial.print(cols[x]); Serial.println(" as input-pullup");
     pinMode(cols[x], INPUT_PULLUP);
   }
     
 }
 
 void readMatrix() {
+  
   // iterate the columns
   for (int colIndex=0; colIndex < colCount; colIndex++) {
-    // col: set to output to low
+    
+    // enable col: set to output and low
     byte curCol = cols[colIndex];
     pinMode(curCol, OUTPUT);
     digitalWrite(curCol, LOW);
 
     // row: interate through the rows
     for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
+      
       byte rowCol = rows[rowIndex];
+      
       pinMode(rowCol, INPUT_PULLUP);
-      keys[colIndex][rowIndex] = digitalRead(rowCol);
+      keys[colIndex][rowIndex] = !digitalRead(rowCol);
       pinMode(rowCol, INPUT);
+      
     }
-    // disable the column
+    
+    // disable the col (set as input)
     pinMode(curCol, INPUT);
+    
+  }
+  
+}
+
+
+void printKeyboard() {
+  for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
+    for (int colIndex=0; colIndex < colCount; colIndex++) { 
+      if (keys[rowIndex][colIndex])
+        Keyboard.press(keyValues[rowIndex][colIndex]);
+      else
+        Keyboard.release(keyValues[rowIndex][colIndex]);
+    } 
   }
 }
+
 
 void printMatrix() {
   for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
@@ -68,4 +100,6 @@ void loop() {
   readMatrix();
   if (Serial.read()=='!')
     printMatrix();
+  printKeyboard();
+  delay(100);
 }
